37eadb8cb82b20af3b5554907e6d432c88f61ccb
AJINKYA <ajinkyacodes@gmail.com>
2025-07-16 22:13:29 +0530

    Student and BankAccount classes created

---
+++ src/LLD_Lecture1/Student.java
@@ -0,0 +1,44 @@
+package LLD_Lecture1;
+
+public class Student {
+    // attributes of the LLD_Lecture1.Student class
+    String name;
+    int id;
+    String email;
+    String course;
+
+    // Behaviours of the student class
+    void registerForCourse(String newCourse){
+        this.course = newCourse;
+        System.out.println(name + " has registered for the course: "+ course);
+    }
+
+    void submitAssignment(String assignmentTitle) {
+        System.out.println(name + " has submitted the assignment: "+ assignmentTitle);
+    }
+
+    void displayDetails() {
+        System.out.println("LLD_Lecture1.Student Details: ");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Email: " + email);
+        System.out.println("Course: " + course);
+    }
+
+    public static void main(String[] args) {
+        // We can create real entities from the LLD_Lecture1.Student class
+        LLD_Lecture1.Student prakhar  = new LLD_Lecture1.Student();
+        prakhar.name = "Prakhar";
+        prakhar.email = "prakhar@gmail.com";
+        prakhar.id = 1;
+        prakhar.course = "Backend LLD";
+        prakhar.displayDetails();
+
+        LLD_Lecture1.Student shilpa  = new LLD_Lecture1.Student();
+        shilpa.name = "Shilpa";
+        shilpa.email = "shilpa@gmail.com";
+        shilpa.id = 1;
+        shilpa.course = "Backend LLD July";
+        shilpa.displayDetails();
+    }
+}
--- src/constructors/Client.java
+++
@@ -1,9 +0,0 @@
-package constructors;
-
-public class Client {
-    public static void main(String[] args) {
-        Student s = new Student();
-        Student s2 = new Student("Akash", 23);
-        System.out.println("DEBUG");
-    }
-}
