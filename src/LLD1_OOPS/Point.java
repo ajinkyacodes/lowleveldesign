664a996f00fccc614884aad7a9f9553a72a7eb44
AJINKYA <ajinkyacodes@gmail.com>
2025-07-17 08:07:29 +0530

    code added

---
+++ src/LLD1_OOPS/Point.java
@@ -0,0 +1,6 @@
+package LLD1_OOPS;
+
+public class Point {
+    int x;
+    int y;
+}
---
+++ src/LLD1_OOPS/Rectangle.java
@@ -0,0 +1,40 @@
+package LLD1_OOPS;
+
+public class Rectangle {
+    // write the code of rectangle class here
+    Point topLeft;
+    int height;
+    int width;
+
+    // Area of Reactangle
+    int getArea() {
+        return this.height * this.width;
+    }
+
+    // Perimeter of Rectangle
+    int getPerimeter() {
+        return 2 * (this.height + this.width);
+    }
+
+    // Bottom Right of the Reacyangle
+    Point getBottomRight() {
+        Point obj = new Point();
+
+        obj.x = this.topLeft.x + this.width;
+        obj.y = this.topLeft.y - this.height;
+        return obj;
+    }
+
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+
+        rect.width = 3;
+        rect.height = 4;
+
+        rect.topLeft = new Point();
+
+        System.out.println(rect.getArea());
+        System.out.println(rect.getPerimeter());
+        System.out.println(rect.getBottomRight());
+    }
+}
