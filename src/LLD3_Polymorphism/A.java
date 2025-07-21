013d32f26dc4b0775f9c9b4579fb631c1f002fd9
AJINKYA <ajinkyacodes@gmail.com>
2025-07-21 22:39:21 +0530

    code added

---
+++ src/LLD3_Polymorphism/A.java
@@ -0,0 +1,7 @@
+package LLD3_Polymorphism;
+
+public class A {
+    void doSomething() {
+        System.out.println("Inside A");
+    }
+}
---
+++ src/LLD3_Polymorphism/Main.java
@@ -0,0 +1,14 @@
+package LLD3_Polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Example of Method Overriding - Runtime Polymorphism
+
+        A a = new A();
+        a.doSomething(); // Inside A
+
+        a = new B();
+        a.doSomething(); // Inside B
+    }
+}
