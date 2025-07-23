ba974b94bee5cd056ae2883de7b6fc2cdbe7feeb
AJINKYA <ajinkyacodes@gmail.com>
2025-07-23 17:14:50 +0530

    code added

---
+++ src/LLD3/Assignments/Point.java
@@ -0,0 +1,11 @@
+package LLD3.Assignments;
+
+public class Point {
+    // write the code of point class here
+    protected int x;
+    protected int y;
+
+    public void display(){
+        System.out.println("["+this.x+", "+this.y+"]");
+    }
+}
