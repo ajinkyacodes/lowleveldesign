8a2005c2c0320a175609d37ed86b31889d1b33c3
AJINKYA <ajinkyacodes@gmail.com>
2025-07-23 23:12:57 +0530

    Created Librarian class in LibrarySystem

---
+++ src/LLD1/Lecture4/LibrarySystem/Librarian.java
@@ -0,0 +1,34 @@
+package LLD1.Lecture4.LibrarySystem;
+
+public class Librarian extends User {
+
+    private String employeeNumber;
+
+    public Librarian(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Librarian(String name, String contactInfo, String employeeNumber) {
+        super(name, contactInfo);
+        this.employeeNumber = employeeNumber;
+    }
+
+    @Override
+    public void displayDashboard() {
+        System.out.println("Librarian Dashboard");
+        System.out.println("Employee Number: "+employeeNumber);
+    }
+
+    @Override
+    public boolean canBorrowBooks() {
+        return true;
+    }
+
+//    public void addNewBook(Book book) {
+//
+//    }
+//
+//    public void removeBook(Book book) {
+//
+//    }
+}
