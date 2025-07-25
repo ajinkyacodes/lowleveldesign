02d19480aef6f9c8da81bb4cc11ba0e396a08591
AJINKYA <ajinkyacodes@gmail.com>
2025-07-25 22:46:26 +0530

    Multithreading demo code added

--- src/LLD1/Lecture5/multithreadingDemo/Client.java
+++ src/LLD1/Lecture5/multithreadingDemo/Client.java
@@ -1,15 +1,28 @@
 package LLD1.Lecture5.multithreadingDemo;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Client {
     public static void main(String[] args) {
 
         System.out.println("Current thread name: " + Thread.currentThread().getName());
 
-        // New thread
-        HelloWorldPrinter helloWorldPrinter = new HelloWorldPrinter();
-        Thread t = new Thread(helloWorldPrinter);
-        t.start();
+        HelloWorldPrinter helloWorldPrinter = new HelloWorldPrinter(10);
+
+        // New thread (Old Way)
+//        Thread t = new Thread(helloWorldPrinter);
+//        t.start();
+
+
+        // Better Way
+        // Using Executor Service - basically using Thread pools
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for(int i=0; i<15; i++) {
+            executorService.submit(helloWorldPrinter);
+        }
 
         System.out.println("DONE!!");
+        executorService.shutdown();
     }
 }
--- src/LLD1/Lecture5/multithreadingDemo/HelloWorldPrinter.java
+++ src/LLD1/Lecture5/multithreadingDemo/HelloWorldPrinter.java
@@ -2,8 +2,15 @@ package LLD1.Lecture5.multithreadingDemo;
 
 public class HelloWorldPrinter implements Runnable {
 
+    private int number;
+
+    public HelloWorldPrinter(int num) {
+        this.number = num;
+    }
+
     @Override
     public void run() {
-        System.out.println("Hello world from new thread: "+ Thread.currentThread().getName());
+        System.out.println("Number is: "+number);
+        System.out.println("Hello world from new thread: "+ Thread.currentThread().getName()+" time: "+System.currentTimeMillis());
     }
 }
