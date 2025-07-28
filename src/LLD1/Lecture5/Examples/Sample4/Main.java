69b411e93ba0ee9826e7970d3e68985560e9a4fd
AJINKYA <ajinkyacodes@gmail.com>
2025-07-28 15:07:55 +0530

    Threads sample4 code added

---
+++ src/LLD1/Lecture5/Examples/Sample4/Main.java
@@ -0,0 +1,10 @@
+package LLD1.Lecture5.Examples.Sample4;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i<100; i++){
+            Thread myThread = new Thread(new NumberPrinter(i));
+            myThread.start();
+        }
+    }
+}
---
+++ src/LLD1/Lecture5/Examples/Sample4/NumberPrinter.java
@@ -0,0 +1,14 @@
+package LLD1.Lecture5.Examples.Sample4;
+
+public class NumberPrinter implements Runnable {
+    private int number;
+
+    public NumberPrinter(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Printing " + number + " from " + Thread.currentThread().getName() + " Time: " + System.currentTimeMillis());
+    }
+}
