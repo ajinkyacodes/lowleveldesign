2ae9749e01fb19253448117c39399b01723c6656
AJINKYA <ajinkyacodes@gmail.com>
2025-07-28 18:46:07 +0530

    Threads sample5 code added

---
+++ src/LLD1/Lecture5/Examples/Sample5/FactorialThread.java
@@ -0,0 +1,38 @@
+package LLD1.Lecture5.Examples.Sample5;
+
+import java.math.BigInteger;
+
+public class FactorialThread extends Thread {
+    private long number;
+    private BigInteger result;
+    private boolean isFinished;
+
+    public FactorialThread(long number) {
+        this.number = number;
+        result = BigInteger.valueOf(0);
+        isFinished = false;
+    }
+
+    @Override
+    public void run() {
+        // Business Logic
+        result = factorial(number);
+        isFinished = true;
+    }
+
+    BigInteger factorial(long n) {
+        BigInteger ans = BigInteger.ONE;
+        for (long i = 2; i <= n; i++) {
+            ans = ans.multiply(BigInteger.valueOf(i));
+        }
+        return ans;
+    }
+
+    BigInteger getResult() {
+        return result;
+    }
+
+    boolean isFinished() {
+        return isFinished;
+    }
+}
---
+++ src/LLD1/Lecture5/Examples/Sample5/Main.java
@@ -0,0 +1,41 @@
+package LLD1.Lecture5.Examples.Sample5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        List<Long> inputNumbers = Arrays.asList(100000000L, 3435L, 35435L, 2324L, 4656L, 23L, 5556L);
+        List<FactorialThread> threads = new ArrayList<>();
+
+        for (long number : inputNumbers) {
+            FactorialThread t = new FactorialThread(number);
+            threads.add(t);
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            t.join(2000);
+        }
+
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            FactorialThread t = threads.get(i); // ith thread object
+            if (t.isFinished()) {
+                System.out.println("Factorial of " + inputNumbers.get(i));
+                System.out.println();
+                System.out.println(t.getResult());
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+            } else {
+                System.out.println("Couldn't complete calc in 2s");
+            }
+        }
+        System.out.println("Main is completed.");
+    }
+}
